62e52b860cf6ce0871e6c4ebcf485fd7fe294e16
lipinnambiar <lipinnambiar@gmail.com>
2020-07-08 18:41:16 +0400

    User data fields and repository updated

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -35,9 +35,12 @@ namespace DatingApp.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddControllers().AddNewtonsoftJson(opt=>{
+                opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;});
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
             services.AddCors();
             services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddScoped<IDatingRepository, DatingRepository>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(
                 options=>{
                     options.TokenValidationParameters = new TokenValidationParameters{
