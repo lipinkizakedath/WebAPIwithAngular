71cc2ef9e11015859281631281a294c727701837
lipinnambiar <lipinnambiar@gmail.com>
2020-07-09 12:39:10 +0400

    End of section 8, AutoMapper added

---
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,22 @@
+using System.Linq;
+using AutoMapper;
+using DatingApp.API.DTOs;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<User, UserForListDto>().ForMember(dest => dest.PhotoUrl, 
+                    opt => opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+                    .ForMember(dst => dst.Age, opt =>opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+            CreateMap<User, UserForDetailsDto>().ForMember(dest => dest.PhotoUrl, 
+                    opt => opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+                    .ForMember(dst => dst.Age, opt =>opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+            CreateMap<Photo, PhotosForDetailedDto>();
+        }
+        
+    }
+}
